37296bebade50d5752243f5e80e5d90d7a7ef721
kittyneverdies <Catofeyevich@yandex.ru>
2024-11-01 17:12:37 +0300

    feat: Improved UI design of growth chart

--- bozenka-frontend/src/components/GrowthChart.jsx
+++ bozenka-frontend/src/components/GrowthChart.jsx
@@ -3,6 +3,7 @@ import {
   LineChart,
   Line,
   XAxis,
+  Label,
   YAxis,
   CartesianGrid,
   Tooltip,
@@ -10,79 +11,92 @@ import {
 } from "recharts";
 import { Box, Typography } from "@mui/joy";
 import Avatar from '@mui/joy/Avatar';
+
+import Divider from '@mui/joy/Divider';
 import Card from '@mui/joy/Card';
 import Grid from '@mui/joy/Grid';
 
+
+
+
+
 export default function TestChart({ data, displayData, }) {
- 
+  
   const CustomLegend = (props) => {
     const { payload } = props;
 
     return (
-      <Box sx={{ 
-        display: "flex", 
-        flexDirection: "row", 
-        justifyContent: "space-between", 
-        alignItems: "flex-start",
-        marginTop: 1
-      }}>
-        <Grid container >
-          {payload.map((item, index) => (
-            <Grid>
-            <Box key={index} sx={{ 
+    <Box sx={{ 
+      display: "flex", 
+      flexDirection: "row", 
+      justifyContent: "space-between", 
+      alignItems: "flex-start",
+      marginTop: 2
+    }}>
+      <Grid container >
+        {payload.map((item, index) => (
+          <Grid>
+          <Box key={index} sx={{ 
+            display: "flex", 
+            flexDirection: "row", 
+            alignItems: "center", marginBottom: 2,
+            marginLeft: 2 
+          }}>
+          
+            {displayData[item.value].icon ? 
+              <Avatar size="md" color="neutral" variant="outlined">
+                {displayData[item.value].icon}
+              </Avatar> 
+              : 
+              <Box sx={{ backgroundColor: item.color, width: 10, height: 10,}} />
+            }
+            <Box sx={{ 
               display: "flex", 
-              flexDirection: "row", 
-              alignItems: "center", marginBottom: 2,
-              marginLeft: 2 
+              flexDirection: "column", 
+              alignItems: "flex-start", ml: 2 ,
             }}>
-            
-              {displayData[item.value].icon ? 
-                <Avatar color="neutral" variant="outlined">{displayData[item.value].icon}</Avatar> 
-                : 
-                <Box sx={{ backgroundColor: item.color, width: 10, height: 10,}} />
-              }
-              <Box sx={{ 
-                display: "flex", 
-                flexDirection: "column", 
-                alignItems: "flex-start", ml: 2 ,
-              }}>
-                <Typography level="title-ms" sx={{lineHeight: 1.3, fontWeight: 'bold'}}>
-                  {displayData[item.value].title ? displayData[item.value].title : item.value}
+              <Typography level="title-xs" sx={{lineHeight: 1.3, fontWeight: 'bold'}}>
+                {displayData[item.value].title ? displayData[item.value].title : item.value}
+              </Typography>
+              {displayData[item.value].description && (
+                <Typography level="body-xs" >
+                  {displayData[item.value].description}
                 </Typography>
-                {displayData[item.value].description && (
-                  <Typography level="body-xs" >
-                    {displayData[item.value].description}
-                  </Typography>
-                )}
-            </Box>
+              )}
           </Box>
-          </Grid>
-        ))}
+        </Box>
         </Grid>
-      </Box>
+      ))}
+      </Grid>
+    </Box>
     );
   };
- const CustomTooltip = ({ active, payload, label }) => {
+
+  const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
       return (
-        <Box sx={{ 
-            backgroundColor: "background.surface", 
-            padding: 2, 
-            border: "1px solid",
-            borderColor: 'background.level3', 
-            borderRadius: "lg",
-            boxShadow: 'xs' 
-        }}>
-          <Typography level="title-xs" sx={{ marginBottom: 1, fontWeight: 'bold' }}>
-            {label}
-          </Typography>
-          {payload.map((item, index) => (
-            <>
+      <Box sx={{ 
+          backgroundColor: "background.surface", 
+          padding: 2, 
+          border: "1px solid",
+          borderColor: 'background.level3', 
+          borderRadius: "lg",
+          boxShadow: 'xs' 
+      }}>
+        <Typography level="title-lg">{label}</Typography>
+        
+        <Divider sx={{ mb: 2, mt: 1 }} />
+        {payload.map((item, index) => (
+          <>
             <Typography key={index} startDecorator={displayData[item.name].icon ? displayData[item.name].icon : ""} level="body-xs">
-              {displayData[item.name] ? displayData[item.name].shortTitle : item.name}: {item.value}
-            </Typography>
+              {displayData[item.name] ? displayData[item.name].shortTitle: item.name}: 
+                <Typography textColor={item.color}>
+                  <b>{item.value}</b>
+                </Typography>
+              </Typography>
             </>
           ))}
+        
         </Box>
       );
     }
@@ -91,18 +105,18 @@ export default function TestChart({ data, displayData, }) {
   };
 
   return (
-    <Card sx={{width: 800, m: 3, height: 300, padding: 2, borderRadius: "lg" }}>
-    <Box sx={{ width: 500, height: 300, padding: 2 }}>
-      <LineChart width={500} height={300} data={data} style={{fontFamily: 'Inter'}}>
+    <Box sx={{ width: 500, height: 300, padding: 2, paddingBottom: 5 }}>
+      <LineChart width={500} height={400} data={data} style={{fontFamily: 'Inter'}}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
-        <YAxis />
+        <XAxis style={{fontSize: 12}} dataKey="name" padding={{ left: 30, right: 30 }} marginBottom={1}>
+          <Label value="Dates of knowngrowth" offset={0} position="bottom" fontSize={12} />
+        </XAxis>
+        <YAxis style={{fontSize: 12}} /> 
         <Tooltip content={<CustomTooltip />} />
         <Legend content={<CustomLegend />} />
         <Line type="monotone" dataKey="members" stroke="#8884d8"/>
         <Line type="monotone" dataKey="views" stroke="#82ca9d" />
       </LineChart>
     </Box>
-    </Card>
   );
 }
