2a653cdb4c0248f67419bc03ffbcebf4f4c55ba1
kittyneverdies <Catofeyevich@yandex.ru>
2024-12-01 00:31:37 +0300

    fix: Register form on center, still need changes

--- bozenka-frontend/src/routes/Register.jsx
+++ bozenka-frontend/src/routes/Register.jsx
@@ -113,7 +113,7 @@ const RegisterPage = () => {
   ];
 
   return (
-    <Box sx={{ maxWidth: 480, mx: 'auto', p: 4, mt: 4, mb: 15 }}>
+    <Box sx={{ maxWidth: 480, mx: 'auto', p: 4}}>
     {alert.open && (
       <Box sx={{ textAlign: 'left', mb: 2 }}>
         <Alert
