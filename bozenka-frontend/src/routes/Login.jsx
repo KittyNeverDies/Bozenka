55eb72d6a47e78f220be356976b13de4c218f0fa
kittyneverdies <Catofeyevich@yandex.ru>
2024-12-01 16:39:56 +0300

    experiment: New loign page layout with phones support.

--- bozenka-frontend/src/routes/Login.jsx
+++ bozenka-frontend/src/routes/Login.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Input, Typography, Card } from '@mui/joy';
+import { Box, Button, Input, Typography, Card, Grid } from '@mui/joy';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LinearProgress from '@mui/joy/LinearProgress';
@@ -110,7 +110,7 @@ const LoginPage = () => {
   ];
 
   return (
-    <Box sx={{ maxWidth: 480, mx: 'auto', p: 4 }}>
+    <Box sx={{ maxWidth:1000, mx: 'auto', p: 4 }}>
       {alert.open && (
         <Box sx={{ textAlign: 'left', mb: 2 }}>
           <Alert
@@ -154,63 +154,48 @@ const LoginPage = () => {
         </Box>
       )}
       <Card sx={{ p: 4, backgroundColor: 'background.body', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.08)' }}>
-        <Typography variant="h2" level="h4" sx={{ fontWeight: 'bold', color: 'text.primary', fontSize: '1.75rem' }}>
-          Login
-        </Typography>
-        <Typography level='body-sm'sx={{ mb: 3 }}>
-          Login into account here, to work with unified community management.
-        </Typography>
-        <form onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          {inputFields.map((field) => (
-            <Input
-              key={field.name}
-              name={field.name}
-              type={field.type}
-              placeholder={field.placeholder}
-              value={formData[field.name]}
-              onChange={handleChange}
-              sx={inputStyles}
-              startDecorator={field.icon}
-              aria-label={`${field.name} input field`}
-              autoComplete={field.autoComplete}
-            />
-          ))}
-          <Button
-            variant="solid"
-            type="submit"
-            sx={{
-              width: '100%',
-              mt: 2,
-              fontWeight: 'bold',
-              p: 1.5,
-              transition: 'background-color 0.3s ease',
-            }}
-          >
-            Login
-          </Button>
-        </form>
-        <Box sx={{ textAlign: 'center', mt: 3 }}>
-          <Typography variant="body2" sx={{ color: '#666', mb: 1 }}>
-            Don't have a account?
-          </Typography>
-          <Button
-            component={Link}
-            to="/register"
-            variant="outlined"
-            sx={{
-              borderColor: '#1976d2',
-              color: '#1976d2',
-              fontWeight: 'bold',
-              p: 1.5,
-              '&:hover': {
-                backgroundColor: '#e3f2fd',
-              },
-              transition: 'background-color 0.3s ease',
-            }}
-          >
-            Go to registration page
-          </Button>
-        </Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h2" level="h4" sx={{ fontWeight: 'bold', color: 'text.primary', fontSize: '1.75rem' }}>
+              Login
+            </Typography>
+            <Typography level='body-sm'sx={{ mb: 3 }}>
+              Login into account here, to work with unified community management.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6} sx={{py: 3}}>
+            <form onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+              {inputFields.map((field) => (
+                <Input
+                  key={field.name}
+                  name={field.name}
+                  type={field.type}
+                  placeholder={field.placeholder}
+                  value={formData[field.name]}
+                  onChange={handleChange}
+                  sx={inputStyles}
+                  startDecorator={field.icon}
+                  aria-label={`${field.name} input field`}
+                  autoComplete={field.autoComplete}
+                />
+              ))}
+              <Typography level='body-xs'>Don't have an account? <Link to='/register/'>Register</Link></Typography>
+              <Button
+                variant="solid"
+                type="submit"
+                sx={{
+                  mt: 2,
+                  fontWeight: 'bold',
+                  p: 1.5,
+                  float: 'left',
+                  transition: 'background-color 0.3s ease',
+                }}
+              >
+                Login
+              </Button>
+            </form>
+          </Grid>
+        </Grid>
       </Card>
     </Box>
   );
