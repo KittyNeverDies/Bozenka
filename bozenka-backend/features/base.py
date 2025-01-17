0a2f5b6f1cba840a724f6da3a91e4e0da025b3d3
kittyneverdies <Catofeyevich@yandex.ru>
2025-01-17 17:34:14 +0300

    feat: Some preparations for backend development

---
+++ bozenka-backend/features/base.py
@@ -0,0 +1,43 @@
+class BasicFeature:
+    """
+    A basic class for all features of bozenka platform.
+    There is described the structure of information, stored in classes of features.
+    Don't touch it unless you know what you're doing.
+    """
+
+
+    settings_name = "Example name of setting"
+    settings_description = "Example description of setting"
+    setting_database_row = None
+
+    platforms_available: dict[str: list | str | dict] = {
+        "telegram": {
+            'commands_hints': {
+                # There stored all command hints of telegram version
+                # Format is  "CommandNameHere": "Command description is here"
+                "example": "Its an example"
+            },
+            'handlers': {
+                #  Format is [Handler, [Filters]]
+                'callback': {},
+                'message': {},
+            }
+        },
+        "discord": {
+            'commands_hints': {
+                # In development right now
+            },
+            'handlers': {
+                #  Format is [Handler, [Filters]]
+            }
+        },
+        "vk": {
+            # In development right now
+            'handlers': {
+                #  Format is [Handler, [Filters]]
+            }
+        }
+    }
+
+
+
