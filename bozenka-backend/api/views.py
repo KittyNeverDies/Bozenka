556ea99a040d7c984505617cc41d58202b3339f6
kittyneverdies catofeyev <85691197+KittyNeverDies@users.noreply.github.com>
2025-02-28 15:00:15 +0300

    feat: Started working on auth (Backend, views.py)

--- bozenka-backend/api/views.py
+++ bozenka-backend/api/views.py
@@ -1,3 +1,52 @@
+import logging
+
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+class AuthViews(viewsets.ViewSet):
+    """
+    ViewsSet for authentication of bozenka platform.
+    Gives ability to access to accounts using email and password
+    """
+
+    @action(detail=False, methods=['post'])
+    def register(self, request) -> Response:
+        """
+        View for registering a new user
+        :param request: Request object
+        :return: Response object
+        """
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+
+        return Response(
+            {
+                'status': 'success',
+                'message': 'Successfully registered.',
+            },
+        )
+
+
+    @action(detail=False, methods=['post'])
+    def login(self, request) -> Response:
+        """
+        View for logging in a user
+        :param request: Request object
+        :return: Response object
+        """
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+
+        return Response(
+            {
+                'status': 'success',
+                'message': 'Successfully logged in.',
+            },
+        )
