3d6c2fd62f7bf59bd30c44f1a5bc05140f919ce8
kittyneverdies catofeyev <85691197+KittyNeverDies@users.noreply.github.com>
2025-03-28 15:37:05 +0300

    misc: Spacing fix in main.jsx

--- bozenka-frontend/src/main.jsx
+++ bozenka-frontend/src/main.jsx
@@ -1,26 +1,27 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/joy/styles';
-import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
-import {AuthProvider} from './context/AuthContext.jsx'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {StyledEngineProvider} from '@mui/joy/styles';
+import {CssVarsProvider} from '@mui/joy/styles';
+import {AuthProvider} from './api/contexts/AuthContext.jsx'
 
 
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import softTheme from './theme.js';
 
 
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <CssVarsProvider defaultMode="system" theme={softTheme}>  
-    <StrictMode>
-        <StyledEngineProvider injectFirst>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </StyledEngineProvider>
-    </StrictMode>
-  </CssVarsProvider>
-  </BrowserRouter>
+    <BrowserRouter>
+        <CssVarsProvider defaultMode="system" theme={softTheme}>
+            <StrictMode>
+                <StyledEngineProvider injectFirst>
+                    <AuthProvider>
+                        <App/>
+                    </AuthProvider>
+                </StyledEngineProvider>
+            </StrictMode>
+        </CssVarsProvider>
+    </BrowserRouter>
 )
